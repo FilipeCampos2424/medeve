393b8ecc897d06054b04f81a26c77c476be90069
FilipeCampos2424 <filipegames2343@gmail.com>
2026-04-07 08:38:15 -0300

    Aula 04 adicionada e finalizada

---
+++ Aula_04/ex_03.php
@@ -0,0 +1,14 @@
+<?php
+$usuarios = [
+    [
+        "usuario" => "Filipe24",
+        "senha" => "12345",
+        "perfil" => "aluno"
+    ],
+    [
+        "usuario" => "Arnaldo67",
+        "senha" => "240456",
+        "perfil" => "professor"
+    ]
+];
+?>
