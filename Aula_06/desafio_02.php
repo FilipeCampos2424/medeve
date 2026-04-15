742a3cdff77a90a75cafb7f69b2d935ec3c15d6e
FilipeCampos2424 <filipegames2343@gmail.com>
2026-04-15 18:12:01 -0300

    Aula_06 adicionada e finalizada

---
+++ Aula_06/desafio_02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Desafio_02</title>
+</head>
+<body>
+<br><br>
+<div class="card text-bg-success">
+<div class="card-body">
+<h5 class="card-title">Saldo atual do Sistema</h5>
+<p class="card-text">R$ 50.000,00</p>
+</div>
+</div>
+    
+</body>
+</html>
